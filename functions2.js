ddc0dd2b40fd5a6eb00753adc0d38afd24e1bcfb
juliherculano <juliherculano@gmail.com>
2023-06-14 20:30:11 -0300

    arrow e default

--- functions2.js
+++ functions2.js
@@ -8,7 +8,7 @@
 
 // executaFuncao(x);
 
-/////////////// CALLBACK
+/////////////// CALLBACK : garantem que uma função não seja executada antes que uma tarefa seja concluída, mas logo depois dessa tarefa ser concluída
 
 // function soma (numero1, numero2) {
 //     return numero1 + numero2;
@@ -34,7 +34,7 @@
 // // = const resultado = executaOperacao(multiplicacao, 3, 4);
 // console.log(resultado)
 
-///////////// ARROW FUNCTIONS
+///////////// ARROW FUNCTIONS : são um tipo de sintaxe utilizada para escrever funções de forma mais condensada
 
 const soma = (n1, n2) => {
     return n1 + n2;
@@ -53,4 +53,29 @@ const divisao = (n1, n2) => {
 
 const elevaAoQuadrado = numero => numero * numero
 
-console.log ()
+console.log(soma(4, 3));
+console.log(subtracao(4, 3));
+console.log(multiplicacao(4,3));
+console.log(divisao(4,3));
+
+
+//////// DEFAULT : Permite definir um valor pré-definido caso o parâmetro não seja preenchido ou receba o valor undefined
+
+const dizerOi = (nome = 'indivíduo') => {
+    return 'Oi, ' + nome;
+};
+
+console.log(dizerOi('Juliana'));
+console.log(dizerOi());
+
+///// Função dentro de uma função:
+
+const x = () => {
+    const y = () => {
+        console.log('aaaaa');
+    };
+    return y
+};
+
+let a = x();
+a();
